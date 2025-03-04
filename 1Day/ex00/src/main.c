9e06dfe332e9fea1cb1d1af9655d086967ba0fdd
Alec Croue <alec.croue@gmail.com>
2025-03-04 16:56:14 +0100

    wip

--- 1Day/ex00/src/main.c
+++ 1Day/ex00/src/main.c
@@ -2,10 +2,14 @@
 
 typedef volatile uint16_t v16_t ;
 
-void	ft_delay(uint16_t delay)
+#ifndef F_CPU
+#define F_CPU 16000000
+#endif
+
+void	ft_delay(v16_t value)
 {
-	for (v16_t i = 0; i < delay; i++) {
-		for (v16_t j = 0; j < (v16_t)1600; j++) {
+	for (v16_t i = 0; i < value; i++) {
+		for (v16_t i = 0; i < 1000; i++) {
 			;
 		}
 	}
@@ -13,10 +17,10 @@ void	ft_delay(uint16_t delay)
 
 int	main(void)
 {
-	DDRB |= (1 << DDB1);
+	DDRB |= (1 << DDB0);
 
 	while (1) {
-		ft_delay((v16_t)500);
-		PORTB ^= (1 << PORTB1);
+		ft_delay(500);
+		PORTB ^= (1 << PORTB0);
 	}
 }
