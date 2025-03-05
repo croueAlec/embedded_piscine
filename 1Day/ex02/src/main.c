6825c337c7fbd0e2d71f9e8ee18367bfc6336dfd
Alec Croue <alec.croue@gmail.com>
2025-03-05 13:59:46 +0100

    ex02 done

--- 1Day/ex02/src/main.c
+++ 1Day/ex02/src/main.c
@@ -4,10 +4,20 @@ int	main(void)
 {
 	DDRB |= (1 << PORTB1);
 
-	TCCR1B |= (1 << WGM12); /* timer on */
-	TCCR1A |= (1 << COM1A0); /* output is OC1A */
+	TCCR1A |= (1 << COM1A1); /* output is OC1A */
 	TCCR1B |= ((1 << CS12)); /* scale of 256 */
-	OCR1A = 31249; /* timer max */
+
+	/* page 142 - 16.4
+	Here we set Waveform Generation to mode 14 */
+	TCCR1A |= (1 << WGM11);
+	TCCR1B |= (1 << WGM13) | (1 << WGM12);
+
+	/* The top is now on ICR1 */
+	ICR1 = 62499; /* Same operation as before but with 1 Hz instead of 2 */
+
+	OCR1A = 6250; /* 10% of ICR1 */
+	/* page 145 - 16.11.9 When the timer reaches OCR1A it will trigger flag OCF1A,
+	this flag will then trigger the LED */
 
 	while (1) {
 		;
